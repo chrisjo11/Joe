52bec6d9a1cba1364a52fd5b34e8a3e32b42dd2a
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2022-12-05 03:41:03 -0500

    created win screen and added button to go back home

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void changeScene()
+    public void changeSceneChase()
     {
         SceneManager.LoadScene("Chase");
+    }
+    public void changeSceneHome()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
